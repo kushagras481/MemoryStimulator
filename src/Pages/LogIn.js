8bd09991c0d6258474c38c9548ab7ee5317b2277
Kushagra Srivastava <kushagras481@gmail.com>
2022-06-05 14:51:24 -0700

    working module, but figure out react-router-native

---
+++ src/Pages/LogIn.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react'
+
+import { View } from 'react-native'
+
+import {
+    //BackgroundDiv
+    TopBackgroundDiv,
+    BottomBackgroundDiv
+} from '../Styles/Pages/LogInStyles.js'
+
+function LogIn(props){
+
+    return(
+        //<BackgroundDiv/>  
+
+        <View>
+            <TopBackgroundDiv/>
+            <BottomBackgroundDiv/>
+        </View> 
+        
+
+    )
+
+}
+
+export default LogIn
---
+++ src/Styles/Pages/LogInStyles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native'
+import * as BaseStyles from '../BaseStyles.js'
+
+export const TopBackgroundDiv = styled(BaseStyles.Container)`
+    background: #292929;
+    box-sizing: border-box;
+    height: 50%50%;
+    width: 100%;
+    position: relative;
+    z-index: 0;
+`
+
+export const BottomBackgroundDiv = styled(BaseStyles.Container)`
+    background: #FFFFFF;
+    box-sizing: border-box;
+    height: 50%;
+    width: 100%;
+    position: relative;
+    z-index: 0;
+`
+
+/*
+export const BackgroundDiv = styled(BaseStyles.Div)`
+    background: #A3CDED
+`
+*/
